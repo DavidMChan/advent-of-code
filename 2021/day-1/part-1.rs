47059aa1b78b27c1f7d32574d25064b7974d4e5b
DavidMChan <david@iridescent.io>
2022-11-30 12:16:33 -0800

    Added 2021 Day 1

---
+++ 2021/day-1/part-1.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    // Load the file from the first argument
+    let filename = env::args().nth(1).expect("No filename given");
+    let contents = fs::read_to_string(filename).expect("Could not read file");
+
+    // Split the file into lines
+    let lines: Vec<&str> = contents.split_whitespace().collect();
+
+    // Each line is an integer, so parse it
+    let numbers: Vec<i32> = lines.iter().map(|x| x.parse().unwrap()).collect();
+
+    // Count the number of increases in the file
+    let mut  num_increases = 0;
+    for i in 1..numbers.len() {
+        if numbers[i-1] < numbers[i] {
+            num_increases += 1;
+        }
+    }
+
+    println!("Number of increases: {}", num_increases);
+}
---
+++ 2021/day-1/part-2.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    // Load the file from the first argument
+    let filename = env::args().nth(1).expect("No filename given");
+    let contents = fs::read_to_string(filename).expect("Could not read file");
+
+    // Split the file into lines
+    let lines: Vec<&str> = contents.split_whitespace().collect();
+
+    // Each line is an integer, so parse it
+    let numbers: Vec<i32> = lines.iter().map(|x| x.parse().unwrap()).collect();
+
+    // Count the number of increases in the file
+    let mut num_increases = 0;
+    let mut prev_window_sum = -1;
+    for i in 2..numbers.len() {
+        if prev_window_sum < 0 {
+            prev_window_sum = numbers[i-2] + numbers[i-1] + numbers[i];
+        } else {
+            let window_sum = numbers[i-2] + numbers[i-1] + numbers[i];
+            if window_sum > prev_window_sum {
+                num_increases += 1;
+            }
+            prev_window_sum = window_sum;
+        }
+    }
+
+    println!("Number of increases: {}", num_increases);
+}
